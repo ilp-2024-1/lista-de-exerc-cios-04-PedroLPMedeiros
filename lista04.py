7480e92636859d93e6955287f3919958ff99a296
PedroLPMedeiros <medeiros.pereira1@escolar.ifrn.edu.br>
2024-08-26 08:13:13 -0300

    questão 5 - finalizada

--- lista04.py
+++ lista04.py
@@ -218,3 +218,80 @@
 # print(f"A média aritmética da lista é: {media_aritmetica}")
 
 ##########################################################################
+
+# 6. Crie um programa que solicite ao usuário uma lista de números inteiros e uma string
+# de mesmo comprimento. O programa deve substituir os números nos índices ímpares
+
+# da lista por caracteres correspondentes da string nos mesmos índices. Exiba a se-
+# quência resultante, separada por um espaço em branco. Exemplo:
+
+# Entrada:
+# 1 2 3 4 5 6
+# abcdef
+
+# Saída:
+# 1 b 3 d 5 f
+
+#RESOLUÇÃO: 
+
+numeros = input("Digite uma lista de números separados por espaço: ").split()
+string = input("Digite uma string com o mesmo número de caracteres: ")
+
+
+if len(numeros) != len(string):
+    print("A lista de números e a string devem ter o mesmo comprimento.")
+else:
+    
+    resultado = []
+    
+    for i in range(len(numeros)):
+        if i % 2 == 1:
+            resultado.append(string[i])
+        else:
+            resultado.append(numeros[i])
+            print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################################
+
+# 8. Escreva um programa que receba como entrada uma string constituída por uma se-
+# quência de números inteiros separados por espaço. O programa deverá transformar
+
+# essa string em uma lista de números inteiros e apresentar o resultado da soma dos
+# valores das posições ímpares dessa lista. Exemplos:
+# Entrada 1:
+# 1 9 3 2 3 9 4
+
+# Saída 1:
+# 9+2+9 = 20
+
+# Entrada 2:
+# 10 40 13 5 8 12 15 13
+
+# Saída 2:
+# 40+5+12+13 = 70
+
+#RESOLUÇÃO: 
+# lista1 = []
+# soma = 0
+# entrada = input("Insira valores de números inteiros em strings separados por espaços: ")
+# for x in entrada:
+#     if x != " ":
+#         lista1 += [int(x)]
+# print(lista1)
+
+# for i in range(1, len(lista1), 2):
+#     soma += lista1[i]
+# print(soma)
+
